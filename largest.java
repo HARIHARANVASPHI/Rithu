4a6f0562f3193140e857e201219b0518360fa9e5
Rithanya27 <rithanya27@gmail.com>
2016-07-03 10:44:25 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -16,15 +16,15 @@ public class largest {
         
        if(a>b && a>c)
        {
-           System.out.println(a+" is gratest among"+a+" " +b+ " "+c);
+           System.out.println(a+" is largest");
        }
        else if(b>c)
        {
-           System.out.println(b+" is gratest among"+a+" " +b+ " "+c); 
+           System.out.println(b+" is largest"); 
        }
        else
        {
-           System.out.println(c+" is gratest among"+a+" " +b+ " "+c);
+           System.out.println(c+" is largest");
        }
    }
 
